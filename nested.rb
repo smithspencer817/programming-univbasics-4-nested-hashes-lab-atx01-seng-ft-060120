7b7cfb76cf9d993cb809edba73cbf81fb9bd0aef
Spencer Smith <smithspencer817@gmail.com>
2020-05-04 22:45:09 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -37,6 +37,7 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
+  programmer_hash[:alan_kay][:known_for]
 
 end
 
@@ -56,6 +57,7 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
+  programmer_hash[:dennis_ritchie][:languages][0]
 
 end
 
